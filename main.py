35043b628c2bab15c48dd7704b56e23c19f2cbbf
Olaghuhfad <158076142+Olaghuhfad@users.noreply.github.com>
2024-03-02 15:58:15 +0000

    Minor edit
    A minor edit removed an import not being used

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import tkinter as tk
-from tkinter import *
 from minesweepergui import MineSweeperGUI
 
-root = Tk()
+root = tk.Tk()
 root.title("Mine Sweeper")
 root.minsize(width=500, height=300)
 
